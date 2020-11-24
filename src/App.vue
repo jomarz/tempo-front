a3ac0696f4c5ad5937814e81f4f9919c4e72faf4
J A MartinezRamirez <jorgemartinez05@gmail.com>
2020-11-24 18:57:31 +0100

    Add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,43 @@
 <template>
-  <NavBar />
+  <div class="content-container">
+    <TopBar />
+    <NavBar />
+  </div>
 </template>
 
 <script>
-import NavBar from './components/NavBar.vue'
+import TopBar from './components/TopBar.vue';
+import NavBar from './components/NavBar.vue';
 
 export default {
   name: 'App',
   components: {
+    TopBar,
     NavBar
   }
 }
 </script>
 
 <style lang="scss">
+  
+  @import url('https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Roboto:wght@300;400;700&display=swap');
+
   html, body {
     font-size: 20px;
   }
   body {
     margin: auto;
-    padding: 0;
-    max-width: 1140px;
+    padding: 0px;
     background-color: white;
     color: black;
   }
+  .content-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    max-width: 1140px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
 </style>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,47 @@
+<template>
+    <div  class="container-fluid" id="navbar">
+        <div class="row">
+            <div class="col-6 logo-container">
+                <a class='navbar-logo'></a>
+            </div>
+            <SearchBox class="col-6"/>
+        </div>
+    </div> 
+</template>
+
+<script>
+import SearchBox from './SearchBox.vue'
+export default {
+    name: 'NavBar',
+    components: {
+        SearchBox
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    #navbar {
+        background-color: white;
+        .row {
+        height: 76px;
+        margin: 65px 20px 55px 20px;
+        }
+        .logo-container {
+            height: 100%;
+            display: inline;
+        }
+        .navbar-logo {
+            display: block;
+            background-image: url('../assets/img/tempo_logo.svg');
+            background-size: contain;
+            background-repeat: no-repeat;
+            height: 100%;
+            img{
+                height: 100%;
+            }
+        }
+        h1 {
+            color: black;
+        }
+    }
+</style>
