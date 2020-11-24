0b96e801374a63eaa6fd7bfe47fd14d7ccc2de98
J A MartinezRamirez <jorgemartinez05@gmail.com>
2020-11-24 12:12:04 +0100

    First commit

---
+++ src/components/SearchBox.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="searchbox-container">
+        <input type="text" />    
+    </div>    
+</template>
+
+<script>
+export default {
+    name: 'SearchBox'
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    .searchbox-container {
+        display: inline;
+    }
+</style>
