a4e50011582cf74abbe54b867049f2a0b080f3f4
J A MartinezRamirez <jorgemartinez05@gmail.com>
2020-11-27 17:49:06 +0100

    Style calendar left box

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,7 +1,6 @@
 <template>
-    <div class="calendar container">
-        <div class="row">
-            <div class="col-3">
+    <div class="calendar row">
+            <div class="col-3 calendar-left-box">
                 <div class="calendar-title-box">
                     <h4 class="calendar-title">CALENDARIO</h4>
                     <div class="calendar-subtitle">DE CONCIERTOS</div>
@@ -22,8 +21,7 @@
             <div class="col-9 days-container">
                 <div class="days-header"></div>
                 <div class="days-numbers"></div>
-            </div>
-        </div>    
+            </div> 
     </div>    
 </template>
 
@@ -35,38 +33,67 @@ export default {
 
 <style lang="scss" scoped>
     .calendar {
-        margin: 40px 0 40px 0;
-        .calendar-title {
-            font-family: 'Roboto', sans-serif;
-            margin-bottom: 0;
-            letter-spacing: 0.08rem;
-        }
-        .calendar-subtitle {
-            font-family: 'Roboto', sans-serif;
-            font-size: 1.05rem;
-            font-weight: 400;
-            letter-spacing: 0.008em;
+        margin-top: 40px;
+        margin-bottom: 40px;
+        .calendar-left-box {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
         }
         .calendar-title-box {
             flex-direction: column;
             display: flex;
             align-items: center;
         }
+        .calendar-title {
+            font-family: 'Roboto', sans-serif;
+            margin-bottom: 0;
+            letter-spacing: 0.08rem;
+            line-height: 120%
+        }
+        .calendar-subtitle {
+            font-family: 'Roboto', sans-serif;
+            font-size: 1.05rem;
+            font-weight: 400;
+            line-height: 120%;
+            letter-spacing: 0.008em;
+            padding-bottom: 15px;
+            border-bottom: solid 1.7px #ffc729;
+        }
         .calendar-month {
             display: flex;
             justify-content: center;
             align-items: center;
+            padding-top: 15px;
         }
         .month-year {
-            margin: 0 15px 0 15px;
+            margin: 0 20px 0 20px;
             display: flex;
             flex-direction: column;
-            align-items: center;            
+            align-items: center;           
         }
         .month-arrow {
             width: 15px;
-            height: 15px;
+            height: 16px;
             line-height: 100%;
+        }
+        .month-name {
+            font-family: 'Roboto', sans-serif;
+            font-size: 1.2rem;
+            font-weight: 700;
+            letter-spacing: 0.04rem;
+            line-height: 1.4rem;
+        }
+        .year {
+            font-family: 'Roboto', sans-serif;
+            font-size: 1.2rem;
+            letter-spacing: 0.18rem;
+            font-weight: 400;
+            line-height: 1.4rem;
+        }
+        .days-container {
+            background-color: #f3f3f4;
+            height: 228px;
         }
     }
 </style>
