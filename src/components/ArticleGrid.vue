aa7f5ec57c037ab32c65d50b45919019bb3877f6
J A MartinezRamirez <jorgemartinez05@gmail.com>
2020-12-07 10:34:52 +0100

    Add article grid component

---
+++ src/components/ArticleGrid.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="article-gid event-grid container">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    
+    setup () {
+        const contentType = "event";
+        return { contentType };
+    }
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
