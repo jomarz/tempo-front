b8225da4d56b52bb3adbfdd235166fd07581604e
J A MartinezRamirez <jorgemartinez05@gmail.com>
2020-12-10 11:35:18 +0100

    Create SectionNav component

--- src/components/ConcertsSection.vue
+++ src/components/ConcertsSection.vue
@@ -1,16 +1,8 @@
 <template>
     <div class="concerts-section container-fluid">
-        <div class="section-nav row">
-            <div class="col-2 section-title-container">
-                <h3 class="section-nav-title">Conciertos</h3>
-            </div>
-            <div class="col-10 section-nav-menu">
-                <ul class="nav-menu section-menu">
-                    <li class="navbar-menu-item"><a href="#" class='navbar-menu-link' data-text='Clásica'>Clásica</a></li>
-                    <li class="navbar-menu-item"><a href="#" class='navbar-menu-link' data-text='Ópera'>Ópera</a></li>
-                    <li class="navbar-menu-item"><a href="#" class='navbar-menu-link' data-text='Jazz'>Jazz</a></li>
-                    <li class="navbar-menu-item"><a href="#" class='navbar-menu-link' data-text='World Music'>World Music</a></li>
-                </ul>
+        <div class="row section-nav-row">
+            <div class="col pl-0 pr-0">
+                <SectionNav />
             </div>
         </div>
         <div class="row calendar-row">
@@ -39,8 +31,9 @@ import AdBox from './AdBox.vue'
 import ArticleGrid from './ArticleGrid.vue'
 import Calendar from './Calendar.vue'
 import ReceiveCTA from './ReceiveCTA.vue'
+import SectionNav from './SectionNav.vue'
 export default {
-  components: { Calendar, AdBox, ArticleGrid, ReceiveCTA },
+  components: { Calendar, AdBox, ArticleGrid, ReceiveCTA, SectionNav },
     
 }
 </script>
@@ -48,27 +41,6 @@ export default {
 <style lang="scss" scoped>
     .concerts-section {
         margin: 40px 0 40px 0;
-        .section-nav {
-            padding: 10px 63px 10px 20px;
-            background-color: #f3f3f4;
-        }
-        .section-nav-menu {
-            display: flex;
-            align-items: center;
-            padding: 10px 0 10px 0;
-        }
-        ul.section-menu {
-            margin: 0;
-            li {
-                font-size: 0.95rem;
-                font-family: 'Roboto', sans-serif;
-                font-weight: 400;
-                line-height: 1.3rem;
-            }
-        }
-        .section-menu li{
-            margin-left: 40px;
-        }
         .calendar-row {
             padding: 40px 0 04px 0;
         }
---
+++ src/components/SectionNav.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container-fluid section-nav">
+        <div class="row">
+            <div class="col-2 section-title-container">
+                <h3 class="section-nav-title">Conciertos</h3>
+            </div>
+            <div class="col-10 section-nav-menu">
+                <ul class="nav-menu section-menu">
+                    <li class="navbar-menu-item"><a href="#" class='navbar-menu-link' data-text='Clásica'>Clásica</a></li>
+                    <li class="navbar-menu-item"><a href="#" class='navbar-menu-link' data-text='Ópera'>Ópera</a></li>
+                    <li class="navbar-menu-item"><a href="#" class='navbar-menu-link' data-text='Jazz'>Jazz</a></li>
+                    <li class="navbar-menu-item"><a href="#" class='navbar-menu-link' data-text='World Music'>World Music</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    
+        .section-nav {
+            padding: 10px 63px 10px 20px;
+            background-color: #f3f3f4;
+        }
+        .section-nav-menu {
+            display: flex;
+            align-items: center;
+            padding: 10px 0 10px 0;
+        }
+        ul.section-menu {
+            margin: 0;
+            li {
+                font-size: 0.95rem;
+                font-family: 'Roboto', sans-serif;
+                font-weight: 400;
+                line-height: 1.3rem;
+            }
+        }
+        .section-menu li{
+            margin-left: 40px;
+        }
+</style>
