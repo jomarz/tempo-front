442621f665fa74fe2defce9d8f529047e9722a87
J A MartinezRamirez <jorgemartinez05@gmail.com>
2020-12-23 12:22:31 +0100

    Remove extra items from section nav

--- src/components/SectionNav.vue
+++ src/components/SectionNav.vue
@@ -7,10 +7,6 @@
             <div class="col-10 section-nav-menu">
                 <ul class="nav-menu section-menu">
                     <li class="navbar-menu-item" v-for="menuItem in menuItems" :key="menuItem"><a :href="menuItem.url" class='navbar-menu-link' :data-text="menuItem.text">{{menuItem.text}}</a></li>
-                    <li class="navbar-menu-item"><a href="#" class='navbar-menu-link' data-text='Clásica'>Clásica</a></li>
-                    <li class="navbar-menu-item"><a href="#" class='navbar-menu-link' data-text='Ópera'>Ópera</a></li>
-                    <li class="navbar-menu-item"><a href="#" class='navbar-menu-link' data-text='Jazz'>Jazz</a></li>
-                    <li class="navbar-menu-item"><a href="#" class='navbar-menu-link' data-text='World Music'>World Music</a></li>
                 </ul>
             </div>
         </div>
