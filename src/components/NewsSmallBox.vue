b4275ca6f5ad34a47ab0deca0005490d79897502
J A MartinezRamirez <jorgemartinez05@gmail.com>
2020-12-27 13:31:19 +0100

    Create news small box component

---
+++ src/components/NewsSmallBox.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="news-small-box">
+        <div class="news-thumbnail">
+            <img src="../assets/img/Pictures/thumbnail2.jpg" alt="">
+        </div>
+        <div class="news-title">Imodipic iissimus</div>
+        <div class="news-text">Bea cullendicid eiur sed qui beatectur, occum andesequi omnihicienes del is dis</div>
+    </div>    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    .news-small-box {
+        
+    }
+</style>
--- src/components/NewsVertical.vue
+++ src/components/NewsVertical.vue
@@ -1,12 +1,17 @@
 <template>
     <div class="news-vertical">
         <h3>Noticias</h3>
+        <news-small-box />
     </div>
 </template>
 
 <script>
+import NewsSmallBox from "./NewsSmallBox.vue";
+
 export default {
-    
+    components: {
+        NewsSmallBox
+    }    
 }
 </script>
 
