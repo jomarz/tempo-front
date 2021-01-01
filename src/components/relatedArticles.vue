f795ba254afb8027b88441fefa72dc6b698b1728
J A MartinezRamirez <jorgemartinez05@gmail.com>
2021-01-01 23:33:29 +0100

    Create relatedArticles component

---
+++ src/components/relatedArticles.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="related-articles">
+
+    </div>    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    .related-articles {
+        display: flex;
+    }
+</style>
