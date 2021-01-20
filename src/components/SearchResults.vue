82faa2c14c70816392e3fdc12f2324778f2b04b8
J A MartinezRamirez <jorgemartinez05@gmail.com>
2021-01-20 20:21:05 +0100

    Close search results nd search input when opening a search result

--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -6,7 +6,7 @@
         <div class="results-content">
             <div class="results-list">
                 <div v-for="result in resultsList" class="search-result" :key="result.id">
-                    <a href="" class="result-title" @click.prevent="setArticle(result.id, result.permalink, result.postType)" >{{result.article_title}}</a>
+                    <a href="" class="result-title" @click.prevent="setArticle(result.id, result.permalink, result.postType); $emit('close-results'); store.toggleMobileSearchbox()" >{{result.article_title}}</a>
                     <div class="result-subtitle">{{result.article_subtitle}}</div>
                 </div>
             </div>
