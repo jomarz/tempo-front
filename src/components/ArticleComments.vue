61cb0a6a2d3bc01da239356502a090f03a085b56
J A MartinezRamirez <jorgemartinez05@gmail.com>
2021-01-31 17:18:26 +0100

    Create send comment input bx

--- src/components/ArticleComments.vue
+++ src/components/ArticleComments.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="article-comments">
         <div class="separator"></div>
+        <div class="comment-add-section">
+            <textarea class="new-comment-input" type="text" placeholder="Escribe tu comentario ..." />
+            <img src="..\assets\img\icons\EnviarIcon.svg" alt="" class="send-icon">
+        </div>
+        <div class="separator"></div>
         <comment />
         <div class="separator"></div>
         <comment />
@@ -20,10 +25,26 @@ export default {
 <style lang="scss" scoped>
     .article-comments {
         margin: 0 0 25px;
+        .comment-add-section {
+            display: flex;
+            align-items: center;
+            .new-comment-input {
+                flex-grow: 1;
+                height: 2rem;
+                border: 1px solid #d1d3d4;
+                border-radius: 5px;
+                margin-right: 5px;
+                padding: 7px 15px;
+                font-size: 0.8rem;
+            }
+            .send-icon {
+                height: 1.2rem;
+            }
+        }
         .separator {
             width: 100%;
             margin: 20px 0;
-            border-top: 1px solid #d1d3d4;
+            border-top: 1px solid #bcbec0;
         }
     }
 </style>
