c36cf63762ad824012ce8af20106c3cbdf1ee9e6
J A MartinezRamirez <jorgemartinez05@gmail.com>
2021-02-02 12:46:36 +0100

    Create CommentRespond component

--- src/components/ArticleComments.vue
+++ src/components/ArticleComments.vue
@@ -8,17 +8,15 @@
             <Comment :comment="comment" />
             <div class="separator"></div>
         </template>
-        <div class="comment-add-section">
-            <textarea class="new-comment-input" type="text" placeholder="Escribe tu comentario ..." />
-            <img src="..\assets\img\icons\EnviarIcon.svg" alt="" class="send-icon">
-        </div>
+        <comment-respond />
     </div>
 </template>
 
 <script>
 import Comment from './Comment.vue'
+import CommentRespond from './CommentRespond.vue';
 export default {
-    components: { Comment },
+    components: { Comment, CommentRespond },
     props: {
         comments: {type: Array}
     },
@@ -41,22 +39,6 @@ export default {
             }
             .close-comments-icon:hover {
                 cursor: pointer;
-            }
-        }
-        .comment-add-section {
-            display: flex;
-            align-items: center;
-            .new-comment-input {
-                flex-grow: 1;
-                height: 2rem;
-                border: 1px solid #d1d3d4;
-                border-radius: 5px;
-                margin-right: 5px;
-                padding: 7px 15px;
-                font-size: 0.8rem;
-            }
-            .send-icon {
-                height: 1.2rem;
             }
         }
         .separator {
---
+++ src/components/CommentRespond.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="comment-respond">
+        <textarea class="new-comment-input" type="text" placeholder="Escribe tu comentario ..." />
+        <img src="..\assets\img\icons\EnviarIcon.svg" alt="" class="send-icon">
+    </div>    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    .comment-respond {
+        display: flex;
+        align-items: center;
+        .new-comment-input {
+            flex-grow: 1;
+            height: 2rem;
+            border: 1px solid #d1d3d4;
+            border-radius: 5px;
+            margin-right: 5px;
+            padding: 7px 15px;
+            font-size: 0.8rem;
+        }
+        .send-icon {
+            height: 1.2rem;
+        }
+    }
+</style>
