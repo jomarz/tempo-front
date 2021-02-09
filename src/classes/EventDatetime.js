227290c5ab77d2d7bbbfd30241532aa719593091
J A MartinezRamirez <jorgemartinez05@gmail.com>
2021-02-09 18:22:36 +0100

    Add getDistanceToEvent method to EventDatetime class

--- src/classes/EventDatetime.js
+++ src/classes/EventDatetime.js
@@ -4,4 +4,25 @@ export default class EventDatetime
     {
         this.datetime = new Date(datetimeString);
     }
+    getDistanceToEvent()
+    {
+        // Get today's date and time
+        var now = new Date().getTime();
+            
+        // Find the distance between now and the count down date
+        var distance = this.datetime.getTime() - now;
+            
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        return {
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
 }
