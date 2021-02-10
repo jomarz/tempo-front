481628e82bbd76af45377b2ce83fee5d97d15d06
J A MartinezRamirez <jorgemartinez05@gmail.com>
2021-02-10 10:35:04 +0100

    create commentReactionAPI class

---
+++ src/classes/CommentReactionAPI.js
@@ -0,0 +1,27 @@
+import TempoAPI from './TempoAPI';
+
+export default class CommentReactionAPI extends TempoAPI
+{
+    constructor()
+    {
+        super();
+        this.likeCommentService = "like_comment";
+        this.dislikeCommentService = "dislike_comment";
+    }
+    likeComment(comment_id, isEvent, handler)
+    {
+        data = {
+            comment_id: comment_id,
+            isEvent: isEvent
+        }
+        this.makeAPICall(this.likeCommentService, data, handler);
+    }
+    dislikeComment(comment_id, isEvent, handler)
+    {
+        data = {
+            comment_id: comment_id,
+            isEvent: isEvent
+        }
+        this.makeAPICall(this.dislikeCommentService, data, handler);
+    }
+}
