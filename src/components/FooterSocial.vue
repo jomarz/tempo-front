230ebeaac5189e102879e92123393e0769a9b283
J A MartinezRamirez <jorgemartinez05@gmail.com>
2021-09-23 10:10:35 +0200

    Link to social networks

--- src/components/FooterSocial.vue
+++ src/components/FooterSocial.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="footer-social">
-        <a href="#"><img src="..\assets\img\icons\Facebook.svg" alt="facebook"></a>
-        <a href="#"><img src="..\assets\img\icons\Instagram.svg" alt="instagram"></a>
-        <a href="#"><img src="..\assets\img\icons\Twitter.svg" alt="twitter"></a>
-        <a href="#"><img src="..\assets\img\icons\Youtube.svg" alt="youtube"></a>
-        <a href="#"><img src="..\assets\img\icons\Spotify.svg" alt="spotify"></a>
+        <a href="https://www.facebook.com/TempoSonidosDelMundo/" target="_blank"><img src="..\assets\img\icons\Facebook.svg" alt="facebook"></a>
+        <a href="https://www.instagram.com/tempo_revista/" target="_blank"><img src="..\assets\img\icons\Instagram.svg" alt="instagram"></a>
+        <a href="https://twitter.com/tempo_musica" target="_blank"><img src="..\assets\img\icons\Twitter.svg" alt="twitter"></a>
+        <a href="https://www.youtube.com/channel/UCvmv2nlzpEPvFrucYlJuLqg" target="_blank"><img src="..\assets\img\icons\Youtube.svg" alt="youtube"></a>
+        <!-- <a href="#" target="_blank"><img src="..\assets\img\icons\Spotify.svg" alt="spotify"></a> -->
     </div>    
 </template>
 
